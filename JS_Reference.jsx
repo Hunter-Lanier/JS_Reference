ad3d1b729e8146e5d2b7d718775e79402fbc3e79
hunterlanier.eth <hlanier97@gmail.com>
2022-04-21 19:22:00 -0500

    Create JS_Reference.jsx

---
+++ JS_Reference.jsx
@@ -0,0 +1,72 @@
+/*Comment*/
+
+/*Data Types*/
+
+var undefined = undefined;
+var nothing = null;
+var boolean = true;
+var boolean = false;
+var string = "Hello World";
+var number = 0;
+var float = 1.0;
+var symbol = symbol("id"); /* Immutable identification */
+var object = { key: value };
+
+/* variable*/
+var variable = 0; /*Can be used throughout entire program*/
+let variable = 0; /*Used within scope of declaration*/
+const variable = 0; /*Unchangeable*/
+
+/*declaring variable*/
+var a;
+
+/*assigning variable*/
+var a = 0;
+
+/*displaying data to console*/
+console.log(data);
+
+/*Java Script declarations are case-sensitive*/
+apple != APPLE;
+
+/*Best Practice for data names is to use camelCase*/
+/*lowercase first word, and uppercase everyword after*/
+var camelCaseIsBestPractice;
+
+/*Operators*/
+
+var additon = 1 + 1;
+var subtraction = 1 - 1;
+var multiplication = 1 * 1;
+var division = 1 + 1;
+var remainder = 10 % 4; /*2*/ /*Commonly used to find a odd or even number*/
+var increment = increment++;
+var increment = decrement--;
+number += 1; /*Plus then equals*/ /*example: let a = 10; a =+ 2; a = 10 + 2; a = 12 */
+number -= 1; /*Subtract then equals*/
+number *= 1; /*Multiply then equals*/
+number /= 1; /*Divide then equals*/
+
+/*Using quotes within string*/
+var string = 'Hello "my" friend!'; /*Hello "my" Friend*/
+var string = "Hello 'my' friend";
+var string = ` "He'llo' "my" 'frie'nd" `; /*allows literal use of " and ' */
+
+/*Esaping with "\" */
+/*
+\'
+\"
+\\
+\n - newline
+\r carriage return\
+\t - tab
+\b - backspace
+\f - form freed 
+*/
+
+/*Concenating Strings*/
+var firstName = "John";
+var lastName = "Doe";
+var fullName = firstName + lastName; /*"John Doe"*/
+
+/*27:50 https://www.youtube.com/watch?v=PkZNo7MFNFg&t=1s */
